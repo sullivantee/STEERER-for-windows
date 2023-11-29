db1d75282b29afe6262d0baf56a1316fd94a0a33
sullivantee <106914190+sullivantee@users.noreply.github.com>
2023-11-29 21:30:13 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,9 +6,14 @@ CONFIG=$1
 CHECKPOINT=$2
 GPUS=$3
 
+# Depend on your conda version, some activate may be located at
+# ~anaconda3/Scripts/activate
+# switch it up, if you feel the need to
 source ~/anaconda3/bin/activate STEERER
 
-# export CUDA_VISIBLE_DEVICES=${GPUS:-"1"}
+# the original author comment out this line,
+# but I am unable to run without this. ¯\_(ツ)_/¯
+export CUDA_VISIBLE_DEVICES=${GPUS:-"1"}
 
 python  tools/test_loc.py \
         --cfg=$CONFIG \
